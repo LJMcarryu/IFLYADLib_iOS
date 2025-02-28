6dfdcdf4fa9c914fb8fcc6bb51258a4010bdc414
LJMcarryu <45089816+LJMcarryu@users.noreply.github.com>
2025-02-28 16:56:06 +0800

    添加库

---
+++ IFLYADLib.framework/Headers/IFLYADLib.h
@@ -0,0 +1,40 @@
+//
+//  IFLYADLib.h
+//  IFLYADLib
+//
+//  Created by admin on 27.2.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IFLYADLib.
+FOUNDATION_EXPORT double IFLYADLibVersionNumber;
+
+//! Project version string for IFLYADLib.
+FOUNDATION_EXPORT const unsigned char IFLYADLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFLYADLib/PublicHeader.h>
+
+
+#pragma mark - model
+#import <IFLYADLib/IFLYAdData.h>
+#import <IFLYADLib/IFLYAdAudioData.h>
+#import <IFLYADLib/IFLYAdDeal.h>
+#import <IFLYADLib/IFLYAdHtmlData.h>
+#import <IFLYADLib/IFLYAdImgVoiceData.h>
+#import <IFLYADLib/IFLYAdMonitorData.h>
+#import <IFLYADLib/IFLYAdVideoData.h>
+
+#pragma mark - biz
+#import <IFLYADLib/IFLYNativeAd.h>
+#import <IFLYADLib/IFLYVideoAd.h>
+#import <IFLYADLib/IFLYSplashAd.h>
+
+#pragma mark - tools
+#import <IFLYADLib/IFLYAdConfig.h>
+#import <IFLYADLib/IFLYAdKeys.h>
+#import <IFLYADLib/IFLYAdTool.h>
+#import <IFLYADLib/IFLYAdError.h>
+#import <IFLYADLib/IFLYAdLog.h>
+
+
