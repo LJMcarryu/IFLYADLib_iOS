e96bbe8a303a9a1aaea958f937a28e91bd210716
jmliu6 <jmliu6@iflytek.com>
2025-03-07 16:22:57 +0800

    上传项目代码，排除 Pod 文件

--- IFLYADLibSimple/IFLYADLibSimple/AppDelegate.m
+++ IFLYADLibSimple/IFLYADLibSimple/AppDelegate.m
@@ -6,6 +6,9 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
+
+#import <IFLYADLib/IFLYADLib.h>
 
 @interface AppDelegate ()
 
@@ -15,22 +18,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    ViewController *rootViewController = [[ViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
+    self.window.rootViewController = navigationController;
+    [self.window makeKeyAndVisible];
+
+    /// 设置是否开启个性化推荐
+    [IFLYAdConfig setPersonalizedEnabled:YES];
+
     return YES;
-}
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
 
 @end
