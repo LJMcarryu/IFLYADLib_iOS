2fe7991604284b53ab7c5e05f9eb032a75ec8a84
LJMcarryu <45089816+LJMcarryu@users.noreply.github.com>
2025-03-07 15:45:28 +0800

    上传项目代码，排除 Pod 文件

---
+++ IFLYADLibSimple/IFLYADLibSimple/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  IFLYADLibSimple
+//
+//  Created by admin on 3.3.25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
