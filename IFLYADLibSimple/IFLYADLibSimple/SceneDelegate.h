3047bc422a141490d64012b747001f3b94aa0482
jmliu6 <jmliu6@iflytek.com>
2025-03-07 15:45:28 +0800

    上传项目代码，排除 Pod 文件

---
+++ IFLYADLibSimple/IFLYADLibSimple/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  IFLYADLibSimple
+//
+//  Created by admin on 3.3.25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ IFLYADLibSimple/IFLYADLibSimple/ViewController.m
@@ -0,0 +1,20 @@
+//
+//  ViewController.m
+//  IFLYADLibSimple
+//
+//  Created by admin on 3.3.25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
